46afd4bdad87dcc71647c66bbafd5c0faadd18fa
Ahmad Salem <ahmadsalem5454@gmail.com>
2020-07-08 12:36:58 -0500

    first commit

--- Main.java
+++ Main.java
@@ -1,24 +1,32 @@
 public class Main {
 
   public static void main(String[] args) {
-
-    int i = 5;
-
-    switch (i) {
-
-      case 0:
-        System.out.println("Zero");
-        break;
-      case 1:
-        System.out.println("One");
-        break;
-      case 3:
-        System.out.println("Three");
-        break;
-      default:
-        System.out.println("Wtf man what is going on in there!");
-        break;
-    }
-    // time to begin the test
+    welcome();
+    divide(100, 25);
+    multiply(4, 20);
+    multiply(56, 90);
+    multiply(120, 897);
+    alexlee();
   }
+
+  public static void welcome() {
+    System.out.println("Welcome to our calculator!");
+  }
+
+  public static void multiply(int a, int b) {
+    System.out.println(a * b);
+  }
+
+  public static void divide(int a, int b) {
+    System.out.println(a / b);
+  }
+
+  public static void alexlee() {
+    System.out.println("This dude is so awesome and he is teaching me alot in how to code!\n Watch his youtube channel!!!");
+  }
+
+  // methods are so much cleaner than having to type everything out
+
 }
+
+// also always use sysout as it print the code much faster
