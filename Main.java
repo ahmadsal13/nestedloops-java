aae7ff1c1dc3957bae390c2cbd72917d5a71384d
Ahmad Salem <ahmadsalem5454@gmail.com>
2020-07-08 21:32:14 -0500

    first commit

--- Main.java
+++ Main.java
@@ -1,32 +1,20 @@
 public class Main {
 
   public static void main(String[] args) {
-    welcome();
-    divide(100, 25);
-    multiply(4, 20);
-    multiply(56, 90);
-    multiply(120, 897);
-    alexlee();
-  }
 
-  public static void welcome() {
-    System.out.println("Welcome to our calculator!");
-  }
+    String name = "Billy Bob Joe";
 
-  public static void multiply(int a, int b) {
-    System.out.println(a * b);
-  }
+    System.out.println("Name: " + name);
+    System.out.println("Uppercase: " + name.toUpperCase());
+    System.out.println("Lowercase:" + name.toLowerCase());
+    System.out.println("First Char:" + name.charAt(0));
+    System.out.println(name.length());
+    System.out.println("Last char:" + name.charAt(12));
+    System.out.println("Substring" + name.substring(10, 13));
 
-  public static void divide(int a, int b) {
-    System.out.println(a / b);
   }
-
-  public static void alexlee() {
-    System.out.println("This dude is so awesome and he is teaching me alot in how to code!\n Watch his youtube channel!!!");
-  }
-
-  // methods are so much cleaner than having to type everything out
 
 }
 
-// also always use sysout as it print the code much faster
+
+  // string methods 
