ed9a6f3ff053cffde1a1723c6bce88225ac62536
Ahmad Salem <ahmadsalem5454@gmail.com>
2020-07-23 18:28:44 -0500

    first commit

--- Main.java
+++ Main.java
@@ -1,32 +1,12 @@
 public class Main {
 
   public static void main(String[] args) {
-    int day = 100;
-    switch (day) {
-      case 1:
-        System.out.println("Sunday");
-        break;
-      case 2:
-        System.out.println("Monday");
-        break;
-      case 3:
-        System.out.println("Tuesday");
-        break;
-      case 4:
-        System.out.println("Wenesday");
-        break;
-      case 5:
-        System.out.println("Thursday");
-        break;
-      case 6:
-        System.out.println("Friday");
-        break;
-      case 7:
-        System.out.println("Saturday");
-        break;
-      default: 
-        System.out.println("Not a valid day!!!");
-    }
+
+    String[] fruits = new String[3];
+    fruits[0] = "Mango";
+    fruits[1] = "Apple";
+    fruits[2] = "Banana";
+    System.out.println(fruits[1]);
   }
-  // how to get the time in Java
+  // ArrayListProjects
 }
